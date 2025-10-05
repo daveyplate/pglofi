0d52e3400ba4953c097ac09eabec471c7cd62596
daveycodez <daveycodez@gmail.com>
2025-10-04 23:09:42 -0700

    Bump pglofi version to 0.0.2
    Updated package.json version for @daveyplate/pglofi to 0.0.2. Minor cleanup in lofi.ts by removing an unnecessary blank line.

--- apps/test/src/lib/lofi.ts
+++ apps/test/src/lib/lofi.ts
@@ -2,5 +2,4 @@ import { createLofi, createPostgrestHooks } from "@daveyplate/pglofi"
 import * as schema from "@/database/schema"
 
 export const lofi = createLofi(schema)
-
 export const pg = createPostgrestHooks(schema)
